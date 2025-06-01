4c8f5664b925ff0a99d55283ed7b83eda927762e
schogges <24444046+schogges@users.noreply.github.com>
2025-06-01 23:01:06 +0200

    fix: subpaths could not resolve
    Signed-off-by: schogges <24444046+schogges@users.noreply.github.com>

--- ui/apps/portal/src/main.ts
+++ ui/apps/portal/src/main.ts
@@ -5,9 +5,9 @@ import App from './App.vue'
 import router from './router'
 
 const components = [
-  ['Button', (await import('@quixsi/components/button')).Button],
-  ['Input', (await import('@quixsi/components/input')).Input],
-  ['Checkbox', (await import('@quixsi/components/checkbox')).Checkbox],
+  ['Button', (async () => (await import('@quixsi/components/button')).Button)()],
+  ['Input', (async () => (await import('@quixsi/components/input')).Input)()],
+  ['Checkbox', (async () => (await import('@quixsi/components/checkbox')).Checkbox)()],
 ] as const
 
 const app = createApp(App)
--- ui/packages/components/src/ui/button/Button.vue
+++ ui/packages/components/src/ui/button/Button.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { Primitive, type PrimitiveProps } from 'reka-ui'
-import { cn } from '#/lib/utils'
+import { cn } from '#lib/utils'
 import { type ButtonVariants, buttonVariants } from '.'
 
 interface Props extends PrimitiveProps {
